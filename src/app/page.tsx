504767aa7c670da7a38e6f1db67558f9fac346e0
Emil Sharier <emil.sharier.dev@gmail.com>
2024-07-07 22:06:14 +0530

    chore: Removing un-used assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { validateRequest } from "@/auth/validate-request";
-import Image from "next/image";
 
 export default async function Home() {
   const { user } = await validateRequest();
 
-  console.log({ user });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <p>Hello</p>
